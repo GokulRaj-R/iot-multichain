c250780a56745725974140fe715e175b86613e71
root <root@mtp.us-central1-a.c.seedbox-299206.internal>
2022-03-05 13:09:35 +0000

    Add feature in node client for receiving data

--- installations-utils/client.sh
+++ installations-utils/client.sh
@@ -1,14 +1,51 @@
 #!/bin/bash
 
+#================================================================
+# HEADER
+#================================================================
+#% SYNOPSIS
+#+    ${SCRIPT_NAME} [-stop]
+#%
+#% DESCRIPTION
+#%    This script configures and runs the multichain client node.
+#%
+#% OPTIONS
+#%    -stop                         Stops client node and deletes all docker images
+#%
+#% EXAMPLES
+#%    ${SCRIPT_NAME}
+#%        Start the multichain client
+#%
+#%    $}SCRIPT_NAME} -stop
+#%        Stop multichain client node and remove all docker images
+#%
+#================================================================
+# END_OF_HEADER
+#================================================================
+
+ #== needed variables ==#
+SCRIPT_HEADSIZE=$(head -200 ${0} |grep -n "^# END_OF_HEADER" | cut -f1 -d:)
+SCRIPT_NAME="$(basename ${0})"
+
+  #== usage functions ==#
+usage() { printf "Usage: "; head -${SCRIPT_HEADSIZE:-99} ${0} | grep -e "^#+" | sed -e "s/^#+[ ]*//g" -e "s/\${SCRIPT_NAME}/${SCRIPT_NAME}/g" ; }
+usagefull() { head -${SCRIPT_HEADSIZE:-99} ${0} | grep -e "^#[%+-]" | sed -e "s/^#[%+-]//g" -e "s/\${SCRIPT_NAME}/${SCRIPT_NAME}/g" ; }
+scriptinfo() { head -${SCRIPT_HEADSIZE:-99} ${0} | grep -e "^#-" | sed -e "s/^#-//g" -e "s/\${SCRIPT_NAME}/${SCRIPT_NAME}/g"; }
+
 if [ $# -eq 1 ]
 then
     if [ $1 == "-stop" ]
     then
         docker-compose -f iot-multichain/docker-compose-client.yml -p iot-client down --rmi all
         rm -r iot-multichain
+    elif [ $1 == "--help" ] 
+    then
+        usagefull
+    elif [ $1 == "-h" ] 
+    then
+        usagefull
     else 
-        echo "Run without flags for running containers"
-        echo "Run with flag -stop for stopping containers"
+        echo "Run ${SCRIPT_NAME} --help for available options"
     fi
     exit
 fi
@@ -28,8 +65,19 @@ apt -y install git
 apt -y install node
 
 echo "Cloning source files"
-git clone 'https://github.com/GokulRaj-R/iot-multichain.git'
-cd iot-multichain/
+# git clone 'https://github.com/GokulRaj-R/iot-multichain.git'
+# cd iot-multichain/
+mkdir iot-multichain
+cd iot-multichain
+git init
+git remote add -f origin 'https://github.com/GokulRaj-R/iot-multichain.git'
+
+echo "multichain/node" >> .git/info/sparse-checkout
+echo "node-client" >> .git/info/sparse-checkout
+echo "keygen.js" >> .git/info/sparse-checkout
+echo "mosquitto.conf" >> .git/info/sparse-checkout
+echo "docker-compose-client.yml" >> .git/info/sparse-checkout
+git pull origin master
 
 echo "Generating public-private key pair"
 arr=( $(node keygen.js) )
@@ -43,9 +91,9 @@ echo "Exporting keys to environment variables"
 export PUBLIC_KEY=$publicKey
 export PRIVATE_KEY=$privateKey
 
-echo "MASTER_NODE=$masterNodeAddress" >> docker-multichain/node/env
-echo "NETWORK_PORT=$networkPort" >> docker-multichain/node/env
-echo "CHAINNAME=$chainName" >> docker-multichain/node/env
+echo "MASTER_NODE=$masterNodeAddress" >> multichain/node/env
+echo "NETWORK_PORT=$networkPort" >> multichain/node/env
+echo "CHAINNAME=$chainName" >> multichain/node/env
 echo "HOST_NAME=$clientName" >> node-client/.env
 echo "PRIVATE_KEY=$privateKey" >> node-client/.env
 
@@ -60,5 +108,3 @@ echo -e "${GRE}$publicKey${NC}"
 
 echo "Running docker containers"
 docker-compose -f docker-compose-client.yml -p iot-client up
-
-
--- installations-utils/master.sh
+++ installations-utils/master.sh
@@ -1,15 +1,105 @@
 #!/bin/bash
 
+#================================================================
+# HEADER
+#================================================================
+#% SYNOPSIS
+#+    ${SCRIPT_NAME} [-stop]
+#%
+#% DESCRIPTION
+#%    This script configures and runs the multichain master node.
+#%
+#% OPTIONS
+#%    -stop                         Stops master node and deletes all docker images
+#%
+#% EXAMPLES
+#%    ${SCRIPT_NAME}
+#%        Start the multichain master
+#%
+#%    $}SCRIPT_NAME} -stop
+#%        Stop multichain master node and remove all docker images
+#%
+#================================================================
+# END_OF_HEADER
+#================================================================
+
+  #== needed variables ==#
+SCRIPT_HEADSIZE=$(head -200 ${0} |grep -n "^# END_OF_HEADER" | cut -f1 -d:)
+SCRIPT_NAME="$(basename ${0})"
+
+  #== usage functions ==#
+usage() { printf "Usage: "; head -${SCRIPT_HEADSIZE:-99} ${0} | grep -e "^#+" | sed -e "s/^#+[ ]*//g" -e "s/\${SCRIPT_NAME}/${SCRIPT_NAME}/g" ; }
+usagefull() { head -${SCRIPT_HEADSIZE:-99} ${0} | grep -e "^#[%+-]" | sed -e "s/^#[%+-]//g" -e "s/\${SCRIPT_NAME}/${SCRIPT_NAME}/g" ; }
+scriptinfo() { head -${SCRIPT_HEADSIZE:-99} ${0} | grep -e "^#-" | sed -e "s/^#-//g" -e "s/\${SCRIPT_NAME}/${SCRIPT_NAME}/g"; }
+
 if [ $# -eq 1 ]
 then
     if [ $1 == "-stop" ]
     then
-        docker-compose -f docker-compose-master.yml -p iot-master down --rmi all
+        docker-compose -f iot-multichain/docker-compose-master.yml -p iot-master down --rmi all
+    elif [ $1 == "--help" ] 
+    then
+        usagefull
+    elif [ $1 == "-h" ] 
+    then
+        usagefull
     else 
-        echo "Run without flags for running containers E.g ./master.sh"
-        echo "Run with flag -stop for stopping containers E.g ./master.sh -stop"
+        echo "Run ${SCRIPT_NAME} --help for available options"
     fi
     exit
 fi
 
+read -p "Client name: " clientName
+read -p "Chain name: " chainName
+
+echo "Updating package sources"
+apt -y update && apt -y upgrade
+
+echo "Installing git, node, docker and docker compose"
+apt -y install docker.io
+apt -y install docker-compose
+apt -y install git
+apt -y install node
+
+echo "Cloning source files"
+# git clone 'https://github.com/GokulRaj-R/iot-multichain.git'
+# cd iot-multichain/
+mkdir iot-multichain
+cd iot-multichain
+git init
+git remote add -f origin 'https://github.com/GokulRaj-R/iot-multichain.git'
+
+echo "multichain/master" >> .git/info/sparse-checkout
+echo "node-master" >> .git/info/sparse-checkout
+echo "node-client" >> .git/info/sparse-checkout
+echo "keygen.js" >> .git/info/sparse-checkout
+echo "mosquitto.conf" >> .git/info/sparse-checkout
+echo "docker-compose-master.yml" >> .git/info/sparse-checkout
+git pull origin master
+
+echo "Generating public-private key pair"
+arr=( $(node keygen.js) )
+# echo ${arr[0]}
+# echo ${arr[1]}
+
+publicKey=${arr[0]}
+privateKey=${arr[1]}
+
+echo "Exporting keys to environment variables"
+export PUBLIC_KEY=$publicKey
+export PRIVATE_KEY=$privateKey
+
+echo "HOST_NAME=$clientName" >> node-client/.env
+echo "PRIVATE_KEY=$privateKey" >> node-client/.env
+
+BLUE='\033[0;34m' # Blue Color
+GRE='\033[0;32m' # Green Color
+NC='\033[0m' # No Color
+
+echo "==============================="
+echo -e "${BLUE}PUBLIC_KEY${NC}"
+echo "==============================="
+echo -e "${GRE}$publicKey${NC}"
+
+echo "Running docker containers"
 docker-compose -f docker-compose-master.yml -p iot-master up
